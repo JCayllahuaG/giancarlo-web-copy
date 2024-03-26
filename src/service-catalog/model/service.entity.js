01c1fc63a23dc25611dc3e3581b178d20563e193
JCayllahuaG <u202019558@upc.edu.pe>
2024-03-26 14:25:39 -0500

    feat: :sparkles: added service entity to handle data storage

--- src/service-catalog/model/service.entity.js
+++ src/service-catalog/model/service.entity.js
@@ -1,26 +1,27 @@
-export class Service{
-    constructor(id,name,port,protocol, published ){
-        this.id = id;
-        this.name = name;
-        this.port = port;
-        this.protocol = protocol;
-        this.published = published;
-        this.status = this.published === true ? 'Published' : 'Unpublished'
-    }
+export class Service {
+  constructor(id, name, port, protocol, published) {
+    this.id = id
+    this.name = name
+    this.port = port
+    this.protocol = protocol
+    this.published = published
+    this.status = this.published === true ? 'Published' : 'Unpublished'
+  }
 
-    static FromDisplayableService(displayableService){
-        return new Service(
-            displayableService.id,
-            displayableService.name,
-            displayableService.protocol,
-            displayableService.status.label === 'Published')
+  static FromDisplayableService(displayableService) {
+    return new Service(
+      displayableService.id,
+      displayableService.name,
+      displayableService.protocol,
+      displayableService.status.label === 'Published'
+    )
+  }
+  static toDisplayableService(service) {
+    return {
+      id: service.id,
+      name: service.name,
+      protocol: service.protocol,
+      status: service.published === true ? 'Published' : 'Unpublished'
     }
-    static toDisplayableService(service){
-        return {
-            id: service.id,
-            name: service.name,
-            protocol: service.protocol,
-            status: service.published === true ? 'Published' : 'Unpublished'
-        }
-    }
+  }
 }
