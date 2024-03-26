e6c42f9866c86b08d09813fdea5bed22f8e76f09
JCayllahuaG <u202019558@upc.edu.pe>
2024-03-26 14:23:37 -0500

    feat: :sparkles: added service component to service catalog context

---
+++ src/service-catalog/components/ServiceTable.component.vue
@@ -0,0 +1,14 @@
+<script setup>
+// import { useToast } from 'primevue/usetoast'
+// const toast = useToast()
+
+// const show = () => {
+//   toast.add({ severity: 'success', summary: 'success', detail: 'Message Content', life: 1500 })
+// }
+</script>
+
+<template>
+  <h1 class="text-3xl text-black text-center">Hi from Services Table</h1>
+</template>
+
+<style></style>
