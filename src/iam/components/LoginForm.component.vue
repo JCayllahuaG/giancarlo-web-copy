a3b27eebc0b9ed609fde933101e650afe1ed306b
JCayllahuaG <u202019558@upc.edu.pe>
2024-03-27 20:17:01 -0500

    feat: :sparkles: Login form component added

--- src/iam/components/LoginForm.component.vue
+++ src/iam/components/LoginForm.component.vue
@@ -1,6 +1,60 @@
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function Login() {
+  // Perform login logic here
+  // Redirect to another route after successful login
+  router.push('/dashboard')
+}
+</script>
 
 <template>
-  <!-- TODO: Implement LoginForm Component -->
-  <form></form>
+  <form
+    class="flex flex-col py-4 bg-primary/60 rounded-2xl items-center w-[30%] h-fit text-white gap-5 border border-[#8c8c8c]/20 drop-shadow-2xl"
+    @submit="Login"
+    @submit.prevent
+  >
+    <h1 class="text-2xl font-normal">Welcome Back</h1>
+
+    <div class="form-field">
+      <label for="email">Username</label>
+      <input autocomplete="off" id="email" type="email" placeholder="example@mail.com" />
+    </div>
+    <div class="form-field">
+      <label for="password">Password</label>
+      <input autocomplete="off" id="password" type="password" placeholder="************" />
+    </div>
+    <div class="submit-section text-white text-lg">
+      <button
+        class="flex w-full h-14 items-center justify-center bg-[#0DB387] rounded-xl px-4 py-2"
+        type="submit"
+      >
+        Log In
+      </button>
+      <span>or</span>
+      <button class="google-button" type="button">
+        <img src="@assets/images/google-icon.webp" width="30px" alt="Google Logo" />
+        <span class="ml-2 text-[#818181] font-normal">Continue with </span>Google
+      </button>
+    </div>
+  </form>
 </template>
+<style scoped>
+.form-field {
+  @apply flex flex-col w-full px-8 space-y-1;
+}
+.form-field label {
+  @apply font-normal;
+}
+.form-field input {
+  @apply min-h-12 rounded-xl bg-primary/70 px-2 placeholder:text-[#afafaf] border border-[#8c8c8c] outline-none;
+}
+.submit-section {
+  @apply flex flex-col items-center w-full gap-2 px-8 py-3;
+}
+.google-button {
+  @apply flex w-full h-14 justify-center items-center gap-2 bg-white rounded-xl px-4 py-2 text-black font-semibold;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ServiceTable from '@service-catalog/components/ServiceTable.component.vue'
+import LoginPage from '@iam/pages/LoginPage.vue'
+import HomePage from '../public/pages/HomePage.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      redirect: '/services'
+      redirect: '/login'
     },
     {
-      path: '/services',
-      name: 'services',
-      component: ServiceTable
+      path: '/login',
+      name: 'login',
+      component: LoginPage
+    },
+    {
+      path: '/dashboard/',
+      name: 'dashboard',
+      component: HomePage,
+      children: [
+        {
+          path: 'services',
+          component: ServiceTable
+        }
+      ]
     }
   ]
 })
