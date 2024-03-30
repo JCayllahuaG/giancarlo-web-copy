4250730dcbf29a798df446271cc520ae1b8905ba
JCayllahuaG <u202019558@upc.edu.pe>
2024-03-30 18:45:13 -0500

    feat: :sparkles: iam bounded context service added

--- src/iam/services/iam-api.service.js
+++ src/iam/services/iam-api.service.js
@@ -1,1 +1,15 @@
-//TODO: Implement Identity & Access Management API service
+import http from '@/shared/services/http-common'
+export class IdentityAccessManagementService {
+  getAllUsers() {
+    return http.get('/users')
+  }
+  getUserById(id) {
+    return http.get(`/users/${id}`)
+  }
+  postUser(userData) {
+    return http.post('/users', userData)
+  }
+  getUserByEmail(email) {
+    return http.get(`/users?email=${email}`)
+  }
+}
